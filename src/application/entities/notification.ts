8cf3425c4274d030584019338d318aacbe217dbe
caduxl007 <eduardo2020dev@gmail.com>
2022-12-13 23:46:07 -0300

    feat: add possibility create notification

--- src/application/entities/notification.ts
+++ src/application/entities/notification.ts
@@ -1,5 +1,6 @@
 import { Replace } from "src/helpers/Replace";
 import { Content } from "./content";
+import { randomUUID } from "node:crypto";
 
 export interface NotificationProps {
   recipientId: string;
@@ -10,13 +11,19 @@ export interface NotificationProps {
 }
 
 export class Notification {
+  private _id: string;
   private props: NotificationProps;
 
   constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
+    this._id = randomUUID();
     this.props = {
       ...props,
       createdAt: props.createdAt ?? new Date(),
-    }
+    };
+  }
+
+  public get id(): string {
+    return this._id
   }
 
   public set recipientId(recipientId: string) {
@@ -36,7 +43,7 @@ export class Notification {
   public set category(category: string) {
     this.props.category = category;
   }
-  public get category(): string { 
+  public get category(): string {
     return this.props.category;
   }
 
