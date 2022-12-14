23e990ab871790eb23b6972cda4a54e6ad45f1ce
caduxl007 <eduardo2020dev@gmail.com>
2022-12-13 23:05:06 -0300

    feat: add repository pattern notification

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,13 +1,24 @@
+import { Notification } from "../entities/notification";
 import { SendNotification } from "./send-notification";
 
+const notifications: Notification[] = [];
+
+const notificationsRepository = {
+  async create(notification: Notification) {
+    notifications.push(notification);
+  },
+};
+
 describe("Notification", () => {
-  it("should be able to create a notification", () => {
-    const notification = new SendNotification().execute({
+  it("should be able to create a notification", async () => {
+    const sendNotification = new SendNotification(notificationsRepository);
+
+    await sendNotification.execute({
       category: "social",
       recipientId: "id",
       content: "content",
     });
 
-    expect(notification).toBeTruthy();
+    expect(notifications).toHaveLength(1);
   });
 });
